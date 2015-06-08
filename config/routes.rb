a9c39c5876563d424cec3392320b3d0756b1d419
Divjot <Divjot65@gmail.com>
2015-06-08 14:54:49 +0000

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,17 @@ Rails.application.routes.draw do
   get 'login'  => 'sessions#new'
   post'login'   => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
    resources :users
    resources :account_activations, only: [:edit]
    resources :password_resets,     only: [:new, :create, :edit, :update]
    resources :microposts,          only: [:create, :destroy]
+   resources :relationships,       only: [:create, :destroy]
+ end
  
 
 
@@ -73,4 +80,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
